e946374f16d7ea6d56164ea3204e62ff257b4d21
João Guilherme <joaoguilhermesg198@gmail.com>
2025-10-07 20:26:13 -0300

    update pipeline

---
+++ scripts/bandit_to_sarif.py
@@ -0,0 +1,14 @@
+sarif = {
+    "version": "2.1.0",
+    "$schema": "https://json.schemastore.org/sarif-2.1.0.json",
+    "runs": [{
+        "tool": {
+            "driver": {
+                "name": "Bandit",
+                "informationUri": "https://bandit.readthedocs.io/",
+                "rules": []
+            }
+        },
+        "results": []
+    }]
+}
